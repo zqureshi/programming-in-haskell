093ee6b3ee8321d7ddc29fd3204242dc8d170afa
Zeeshan Qureshi <zee@zqureshi.in>
2019-06-01 23:19:00 -0400

    [CH08] Exercise 1 - Nat

---
+++ 08-declaring-types-and-classes/ex1-nat.hs
@@ -0,0 +1,19 @@
+data Nat = Zero | Succ Nat
+           deriving Show
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n-1))
+
+add :: Nat -> Nat -> Nat
+add Zero n = n
+add (Succ m) n = Succ (add m n)
+
+mult :: Nat -> Nat -> Nat
+mult n Zero = Zero
+mult n (Succ Zero) = n
+mult n (Succ m) = add n (mult n m)
